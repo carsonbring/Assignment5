fa65b23bf737dd90e1a27cb810a4447c71191737
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2023-11-28 13:47:25 -0600

    added SeedData and Model Structure for Artist and Song:

---
+++ Assignment5/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment5.Models
+{
+    public class Artist
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+    }
+}
