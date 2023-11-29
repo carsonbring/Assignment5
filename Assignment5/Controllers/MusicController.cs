c41258fdd5ade20ac05798f9774cce6a3ff965d5
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2023-11-28 18:23:45 -0600

    Customer song grid view working

--- Assignment5/Controllers/MusicController.cs
+++ Assignment5/Controllers/MusicController.cs
@@ -1,7 +1,53 @@
-﻿namespace Assignment5.Controllers
-{
-    public class MusicController
-    {
+﻿// MusicController.cs
 
+using Microsoft.AspNetCore.Mvc;
+using Assignment5.Data;
+using Assignment5.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+
+public class MusicController : Controller
+{
+    private readonly Assignment5Context _context;
+
+    public MusicController(Assignment5Context context)
+    {
+        _context = context;
     }
+    public IActionResult Index(string? selectedGenre, int? selectedArtistId)
+    {
+        var distinctGenres = _context.Artist.Select(a => a.Genre).Distinct().ToList();
+        ViewBag.Genres = distinctGenres;
+        ViewBag.SelectedGenre = selectedGenre;
+
+        IEnumerable<Song> songs = null;
+
+        if (!string.IsNullOrEmpty(selectedGenre))
+        {
+            var artists = _context.Artist.Where(a => a.Genre == selectedGenre).ToList();
+            ViewBag.Artists = artists;
+            ViewBag.SelectedArtistId = selectedArtistId;
+            if (selectedArtistId != null)
+            {
+                songs = _context.Song
+                    .Where(s => s.ArtistId == selectedArtistId)
+                    .ToList();
+            }
+            else
+            {
+                // If no artist is selected, show all songs for the selected genre
+                songs = _context.Song
+                    .Where(s => s.Artist.Genre == selectedGenre)
+                    .ToList();
+            }
+        }
+
+        // Other logic...
+
+        return View(songs);
+    }
+   
+    
+
 }
